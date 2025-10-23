c73ae34f7a9bc0e886ad77dd54cd73b082e2d131
Carlos <howtobeastupid@gmail.com>
2025-10-23 00:29:32 -0400

    deleted old components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { DjPackagesComponent } from './pages/dj-packages/dj-packages.component';
-import { PhotoBoothPackagesComponent } from './pages/photo-booth-packages/photo-booth-packages.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { DjGalleryComponent } from './pages/dj-gallery/dj-gallery.component';
 import { PhotoBoothGalleryComponent } from './pages/photo-booth-gallery/photo-booth-gallery.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
-import { AddeventComponent } from './pages/addevent/addevent.component';
-import { MusicRequestComponent } from './components/music-request/music-request.component';
 import { ItservicesService } from './services/itservices.service';
 import { ServicesComponent } from './pages/services/services.component';
 import { BlogSingleComponent } from './pages/blog-single/blog-single.component';
@@ -20,13 +16,11 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'services', component: ServicesComponent },
-  { path: 'photo-booth-packages', component: PhotoBoothPackagesComponent },
   { path: 'blogs', component: BlogComponent },
   { path: 'gallery', component: GalleryComponent },
   { path: 'dj-gallery', component: DjGalleryComponent },
   { path: 'photo-booth-gallery', component: PhotoBoothGalleryComponent },
   { path: 'blog-single/:id', component: BlogSingleComponent },
-  { path: 'addevent', component: AddeventComponent },
   { path: '**', redirectTo: '' }
 ];
 
