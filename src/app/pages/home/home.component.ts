6a623cbd041918e11ca7d126ce5d02e3e596c693
Carlos <howtobeastupid@gmail.com>
2026-01-22 22:53:32 -0500

    DA DA DA DA Da...  im loving it

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { LatestArticlesComponent } from './latest-articles/latest-articles.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, LatestArticlesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/pages/home/latest-articles/latest-articles.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common'; // optimization
+import { RouterModule } from '@angular/router';
+import { BlogService } from '../../../services/blog.service';
+import { Blog } from '../../../interfaces/blog';
+
+@Component({
+    selector: 'app-latest-articles',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './latest-articles.component.html',
+    styleUrl: './latest-articles.component.scss'
+})
+export class LatestArticlesComponent implements OnInit {
+    latestBlogs: Blog[] = [];
+    isLoading = true;
+
+    private blogService = inject(BlogService);
+
+    ngOnInit(): void {
+        this.blogService.viewBlog().subscribe({
+            next: (blogs) => {
+                // Filter for "Tech" category
+                const techBlogs = blogs.filter(blog => blog.catBlog?.toLowerCase() === 'tech');
+
+                // Take the latest 2
+                this.latestBlogs = techBlogs.slice(0, 2);
+                this.isLoading = false;
+            },
+            error: (err) => {
+                console.error('Failed to load blogs', err);
+                this.isLoading = false;
+            }
+        });
+    }
+}
