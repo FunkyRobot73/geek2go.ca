02da0dd6d9743ecbc17ced2baa526e45bb21bd6a
Carlos <howtobeastupid@gmail.com>
2026-04-08 17:03:59 -0400

    branding changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [],
+    providers: [
+      provideRouter([]),
+      provideHttpClient(),
+      provideHttpClientTesting()
+    ],
     declarations: [AppComponent]
 }));
 
@@ -14,16 +19,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'funky-robot'`, () => {
+  it(`should have as title 'Geek2Go.ca'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('funky-robot');
+    expect(app.title).toEqual('Geek2Go.ca');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('funky-robot app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Geek2Go.ca app is running!');
   });
 });
