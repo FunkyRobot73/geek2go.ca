4d55af1d3273d796a850a8c72d012ca01f1967f2
Carlos <howtobeastupid@gmail.com>
2026-04-09 13:21:44 -0400

    more visual changes to chess

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LatestArticlesComponent } from './latest-articles/latest-articles.component';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, LatestArticlesComponent],
+  imports: [CommonModule, RouterModule, LatestArticlesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  profile: any;
 
+  constructor(private profileService: ProfileService) {
+    this.profileService.profile$.subscribe(data => {
+      this.profile = data;
+    });
+  }
 }
